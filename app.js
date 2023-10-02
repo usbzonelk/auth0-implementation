c63b6048437e03943c813bb3f556ca77727b992d
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-10-03 00:56:49 +0530

    Added dbConnection to app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 
+const dbConnection = require("./config/DBConnection").dbConnection;
+
 const app = express();
 
-const startServer = () => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server Fired up on http://127.0.4.5:${process.env.PORT}`);
-  });
+const startServer = async () => {
+   dbConnection
+    .sync()
+    .then(() => {
+      app.listen(process.env.PORT, () => {
+        console.log(`Server Fired up on http://127.0.4.5:${process.env.PORT}`);
+      });
+    })
+    .catch((err) => {
+      console.error("Error syncing Sequelize:", err);
+    });
 };
 
 startServer();
