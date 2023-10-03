86877316464edde4b250f85bec6b0be3130bf5d4
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-10-04 00:40:52 +0530

    Added basic sanitization

--- routes/reserve.js
+++ routes/reserve.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 const dotenv = require("dotenv").config();
 const { Op } = require("sequelize");
 const Reservation = require("../models/Reservation");
+const sanitizer = require("../utils/sanitize");
 
 router.get("/", async (req, res) => {
   const userAuth = { auth: false };
@@ -30,5 +31,50 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/", async (req, res) => {
+  const userAuth = { auth: false };
+  const reservationData = req.body;
+
+  for (const key in reservationData) {
+    reservationData[key] = sanitizer(reservationData[key]);
+  }
+  console.log(reservationData);
+  if (req.oidc.isAuthenticated()) {
+    userAuth.auth = true;
+    userAuth.id = req.oidc.user.sub;
+
+    const options = {
+      method: "GET",
+      url: `${process.env.AUTH0_ISSUER_BASE_URL}/api/v2/users/${userAuth.id}`,
+      headers: { authorization: `Bearer ${process.env.AUTH0_MGMT}` },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        userAuth.userDetails = response.data;
+      })
+      .catch(function (error) {
+        res.redirect("/logout");
+      });
+
+    try {
+      const reservations = await Reservation.findAll({
+        where: {
+          username: {
+            [Op.eq]: userAuth.userDetails.username,
+          },
+        },
+      });
+      userAuth.reservations = reservations;
+      res.render("reserve", { userAuth });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      res.render("dbError");
+    }
+  } else {
+    res.redirect("/login");
+  }
+});
 
 module.exports = router;
