f8fd6eababe0245ddb2bbb79e99d4b86c2a95063
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-10-03 18:00:33 +0530

    Added the user data retrieval

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+const dotenv = require("dotenv").config();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   const userAuth = { auth: false };
   if (req.oidc.isAuthenticated()) {
     userAuth.auth = true;
     userAuth.id = req.oidc.user.sub;
+
+    const options = {
+      method: "GET",
+      url: `${process.env.AUTH0_ISSUER_BASE_URL}/api/v2/users/${userAuth.id}`,
+      headers: { authorization: `Bearer ${process.env.AUTH0_MGMT}` },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        userAuth.userDetails = response.data;
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+
     res.render("dashboard", { userAuth });
   } else {
     res.redirect("/login");
